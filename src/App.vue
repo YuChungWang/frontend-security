317fe1b43842827033f111894ddb3ae88b90f747
Charlie Wang <charliewang324@gmail.com>
2022-12-15 17:26:14 +0800

    ADD: Default demo

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="javascript:alert(`access_token: ${localStorage.getItem('access_token')}`)">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <p>Frontend security demo</p>
+
+    <!-- xss attack -->
+    <button class="btn" @click="submitCounter1 += 1">Submit(XSS Attack)</button>
+    <div v-if="Boolean(submitCounter1)" :key="submitCounter1" v-html="innerHtml1" class="content" />
+
+    <!-- prevent xss attack -->
+    <button class="btn" @click="submitCounter2 += 1">Submit</button>
+    <div v-if="submitCounter2" :key="submitCounter2" v-html="$xss(innerHtml2)" class="content" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const accessToken = '12kfau9xfdzlvlg129a=f38fskuxvhqwr';
+const submitCounter1 = ref(0);
+const innerHtml1 = `<img src='../ww.png' onerror="alert('ACCESS_TOKEN: ${accessToken}')" />`;
+
+const submitCounter2 = ref(0);
+const innerHtml2 = `<img src='../ww.png' onerror="alert('ACCESS_TOKEN: ${accessToken}')" />`;
+</script>
 
 <style scoped>
 .logo {
@@ -22,10 +29,14 @@ import HelloWorld from './components/HelloWorld.vue'
   padding: 1.5em;
   will-change: filter;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
+
+.btn {
+  margin: 0 10px;
+  background: #666666;
 }
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.content,
+:deep(img) {
+  display: none;
 }
 </style>
